2e6fd5f51e4f4966545f47ff36ba85e58c0cfa7d
Rohit-Pratap <>
2023-03-25 00:47:20 +0530

    add emotions service

---
+++ emotion/emotion/__init__.py
@@ -0,0 +1,28 @@
+import logging
+
+import azure.functions as func
+from transformers import RobertaTokenizerFast, TFRobertaForSequenceClassification, pipeline
+
+tokenizer = RobertaTokenizerFast.from_pretrained("arpanghoshal/EmoRoBERTa")
+model = TFRobertaForSequenceClassification.from_pretrained(
+    "arpanghoshal/EmoRoBERTa")
+
+emotion = pipeline('sentiment-analysis', model='arpanghoshal/EmoRoBERTa')
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    logging.info('Python HTTP trigger function processed a request.')
+
+    req_body = req.get_json()
+    text = req_body.get('text')
+
+    emotion_labels = emotion(text)
+    print(emotion_labels)
+    if text:
+        return func.HttpResponse(f"Text: {text}, \nEmotion, {emotion_labels}")
+    else:
+        return func.HttpResponse(
+            "This HTTP triggered function executed successfully. Pass a text in the request body for emotion.",
+            status_code=400
+        )
